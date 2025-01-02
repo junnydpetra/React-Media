8866413546e98f05847f2ae07ba19493cbf88971
junnydpetra <junnycmenezes@gmail.com>
2025-01-02 17:36:12 -0300

    additional modules

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Banner from "./components/Banner";
+import Container from "./components/Container";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Banner image="home" />
+      <Container>
+        <h1>Hello, World!</h1>
+        <p>ReactJS Learning!</p>
+      </Container>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css"
+
+function Footer() {
+    return(
+        <footer className={styles.footer}>
+            <h2>ReactMedia &copy; - Powered by Junny 2025</h2>
+        </footer>
+    );
+}
+
+export default Footer;
